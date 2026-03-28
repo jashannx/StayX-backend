edf70315d67befbb4fabe16ed81faab000212ba2
Jashandeep Singh <jashannx@gmail.com>
2026-03-28 20:18:03 +0530

    dfdf

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -6,8 +6,8 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const getCookieOptions = () => ({
   httpOnly: true,
-  secure: isProduction,
-  sameSite: isProduction ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000,
 });
 
